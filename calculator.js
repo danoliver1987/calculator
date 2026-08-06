e076a3f38f0662644db9a4876496ef8786bb6083
Daniel Oliver <danieloliver19871@icloud.com>
2026-08-06 19:00:44 +0100

    Add snarky error message when dividing by 0

--- calculator.js
+++ calculator.js
@@ -11,8 +11,12 @@ const multiply = function(a, b) {
 }
 
 const divide = function(a, b) {
-    return a / b;
-}
+    if (b === 0) {
+        return "What a plonker!" 
+    }
+    else { 
+        return a / b;
+}}
 
 let num1 = "";
 let num2 = "";
@@ -62,9 +66,14 @@ operatorButtons.forEach(function(button2) {
 
         if (num2 !== "") {
             num1 = sumUp(parseInt(num1), parseInt(num2), operator);
-             num2 = "";
+            if (num1 === "What a plonker!") {
+                return display.value = "What a plonker!";
         };
-
+        
+        num2 = "";
+        
+        }
+        
         operator = btnListener;
         display.value = operator;
     });
@@ -74,10 +83,19 @@ const sumButton = document.querySelector(".sumup");
 
 sumButton.addEventListener("click", function(event) {
     const display = document.querySelector("input");
+
+    if (num1 === "" || num2 === "" || operator === undefined) {
+        return display.value = "Try again!";
+    }
+
     const result = sumUp(parseInt(num1), parseInt(num2), operator);
-    let n = result.toFixed(4);
-    n = parseFloat(n);
-    display.value = n;
+
+    if (result === "What a plonker!") {
+        return display.value = "What a plonker!"
+    } 
+        let n = result.toFixed(4);
+        n = parseFloat(n);
+        display.value = n;
 });
 
 const clearBtn = document.querySelector(".clearBtn");
